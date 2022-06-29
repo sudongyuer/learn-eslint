7805e468a60c231fadd9e770ea77e11839664f78
sudongyu <976499226@qq.com>
2022-06-30 00:27:34 +0800

    chore: update config.ts

--- packages/learn-eslint-doc/docs/.vitepress/config.ts
+++ packages/learn-eslint-doc/docs/.vitepress/config.ts
@@ -99,6 +99,10 @@ function getGuideSidebar() {
     {
       text:'如何安装并使用ESLintConfig',
       link:'/guide/如何安装并使用ESLintConfig'
+    },
+    {
+      text:'创建一个可共享的ESLint插件',
+      link:'/guide/创建一个可共享的ESLint插件'
     }
   ]
 }
