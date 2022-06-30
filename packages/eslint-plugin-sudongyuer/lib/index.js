7ac8ecb561f27a641c351de81efeb5db30ee4c61
sudongyu <976499226@qq.com>
2022-06-30 13:49:20 +0800

    chore: update index.js

--- packages/eslint-plugin-sudongyuer/lib/index.js
+++ packages/eslint-plugin-sudongyuer/lib/index.js
@@ -18,5 +18,12 @@ console.log(requireIndex(__dirname + "/rules"))
 // import all rules in lib/rules
 module.exports.rules = requireIndex(__dirname + "/rules");
 
+module.exports.configs={
+  sudongyuerConfig:{
+    rules:{
+      "sudongyuer/find-todo":["error","todo","fixme"]
+    }
+  }
+}
 
 
