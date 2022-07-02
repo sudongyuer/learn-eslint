f2eda985dca0e8e517fe3524bb2fa4026b287422
sudongyu <976499226@qq.com>
2022-07-02 18:05:24 +0800

    feat: add shared eslint config

---
+++ packages/eslint-config-sudongyuer/index.js
@@ -0,0 +1,12 @@
+module.exports={
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node":true
+    },
+      "rules": {
+        "import/no-unresolved":0,
+        "semi":[2,"never"],
+        "quotes":[2,"single"]
+    }
+}
